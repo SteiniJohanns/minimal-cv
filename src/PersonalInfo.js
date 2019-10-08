48d5c508e4972db0a4bf92fd1c15ecfb79f1aa28
Aðalsteinn Jóhannsson <aj1.h18@nemandi.ntv.is>
2019-10-08 23:20:15 +0000

    Ekkert spes

---
+++ src/PersonalInfo.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const info = () => {
+  return (
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero iste
+      dolores nam! Recusandae tempore, aut, est esse molestiae dolorum laborum
+      earum aperiam repellat nisi eligendi sequi reiciendis, sapiente sunt
+      impedit.
+    </p>
+  );
+};
+
+export default info;
